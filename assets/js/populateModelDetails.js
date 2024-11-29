e1835388c5572169e16187d7cda13b2c94eca99e
theekshana-dilshan <sptheekshanadilshan@gmail.com>
2024-11-29 14:26:25 +0530

    View, Update models

--- assets/js/populateModelDetails.js
+++ assets/js/populateModelDetails.js
@@ -18,3 +18,25 @@ function populateCropDetails(code, commonName, scientificName, image, category, 
     document.getElementById('editCropSeason').value = season;
     document.getElementById('editCropField').value = field;
 }
+
+function populateEquipmentDetails(id, name, type, status, staff, field) {
+    document.getElementById('editEquipmentId').value = id;
+    document.getElementById('editEquipmentName').value = name;
+    document.getElementById('editEquipmentType').value = type;
+    document.getElementById('editEquipmentStatus').value = status;
+    document.getElementById('editStaffDetails').value = staff;
+    document.getElementById('editFieldDetails').value = field;
+}
+
+function populateStaffDetails(id, firstName, lastName, designation, gender, joinedDate, dob, address, contact, email, role, field, vehicle) {
+    document.getElementById('editStaffId').value = id;
+    document.getElementById('editFirstName').value = firstName;
+    document.getElementById('editLastName').value = lastName;
+    document.getElementById('editDesignation').value = designation;
+    document.getElementById('editGender').value = gender;
+    document.getElementById('editJoinedDate').value = joinedDate;
+    document.getElementById('editDOB').value = dob;
+    document.getElementById('editAddress').value = address;
+    document.getElementById('editContact').value = contact;
+    document.get
+}
